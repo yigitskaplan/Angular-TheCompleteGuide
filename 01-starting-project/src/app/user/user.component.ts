730c88b1948560d4b04a618b626686ba8bdb150c
Yiğit Şevki Kaplan <yigitkaplan2004@hotmail.com>
2025-08-26 14:22:08 +0300

    progress: up to section 2/40 completed, TypeScript: Type Aliases & Interfaces

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,5 +1,18 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
+// type User = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// }
+
+interface User{
+  id: string;
+  avatar: string;
+  name: string;
+}
+
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -7,14 +20,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  // @Input({required: true}) id!: string;
-  // @Input({required: true}) avatar!: string;
-  // @Input({required: true}) name!: string;
-  @Input({required: true}) user!: {
-    id: string;
-    avatar: string;
-    name: string;
-  }
+  @Input({required: true}) user!: User;
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
