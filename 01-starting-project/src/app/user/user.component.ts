37315a884c0bcf53789c43dcf603857733292da8
Yiğit Şevki Kaplan <yigitkaplan2004@hotmail.com>
2025-08-26 13:46:49 +0300

    progress: up to section 2/38 completed, pending inputs as objects implementation

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -10,8 +10,12 @@ export class UserComponent {
   @Input({required: true}) id!: string;
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
-  // @Output() select = new EventEmitter();
-  select = output<string>();
+  // @Input({required: true}) user: {
+  //   id: string;
+  //   avatar: string;
+  //   name: string;
+  // }
+  @Output() select = new EventEmitter<string>();
 
   get imagePath() {
     return `assets/users/` +this.avatar;
