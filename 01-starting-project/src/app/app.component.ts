89973eb0569d050c431a4b93bdac2d025d8a43b5
Yiğit Şevki Kaplan <yigitkaplan2004@hotmail.com>
2025-08-26 15:40:49 +0300

    progress: up to section 2/45 completed, Outputting User-specific Tasks

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { NgFor, NgIf } from '@angular/common';
 
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
@@ -9,7 +8,7 @@ import { TasksComponent } from "./tasks/tasks.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
