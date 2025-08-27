cc1e04fa185ba397765b8a85d4171a4c34b4e4bf
Yiğit Şevki Kaplan <yigitkaplan2004@hotmail.com>
2025-08-27 13:53:44 +0300

    progress: up to section 2/47 completed, Storing Data Models in Separate Files

--- 01-starting-project/src/app/tasks/task/task.component.ts
+++ 01-starting-project/src/app/tasks/task/task.component.ts
@@ -1,13 +1,6 @@
 import { Component, Input } from '@angular/core';
 
-
-interface Task {
-  id: string;
-  userId: string;
-  title: string;
-  summary: string;
-  dueDate: string;
-}
+import { type Task } from "./task.models";
 
 @Component({
   selector: 'app-task',
--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,17 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
-// type User = {
-//   id: string;
-//   avatar: string;
-//   name: string;
-// }
-
-interface User{
-  id: string;
-  avatar: string;
-  name: string;
-}
-
+import { type User } from "./user.models";
 
 @Component({
   selector: 'app-user',
