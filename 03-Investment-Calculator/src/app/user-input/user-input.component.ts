45f7bdf2164f65029a9f709be286147efcfacbd7
Yiğit Şevki Kaplan <yigitkaplan2004@hotmail.com>
2025-08-28 16:14:05 +0300

    progress: up to section 4/79 completed, Extracting Values with Two-Way-Binding

--- 03-Investment-Calculator/src/app/user-input/user-input.component.ts
+++ 03-Investment-Calculator/src/app/user-input/user-input.component.ts
@@ -9,7 +9,16 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '5';
+  enteredInvestmentDuration = '10';
+
   onSubmit() {
-    // Handle form submission
+    console.log('Submitted!');
+    console.log(this.enteredInitialInvestment);
+    console.log(this.enteredAnnualInvestment);
+    console.log(this.enteredExpectedReturn);
+    console.log(this.enteredInvestmentDuration);
   }
 }
