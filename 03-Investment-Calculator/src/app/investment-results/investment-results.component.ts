733be7857d886ff0c8bef6923e2b8fd8be69e3a5
Yiğit Şevki Kaplan <yigitkaplan2004@hotmail.com>
2025-08-29 09:06:44 +0300

    progress: up to section 4/85 completed, formating with pipes done

--- 03-Investment-Calculator/src/app/investment-results/investment-results.component.ts
+++ 03-Investment-Calculator/src/app/investment-results/investment-results.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 
 import { type InvestmentResult } from './investment-result.model';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-investment-results',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css'
 })
