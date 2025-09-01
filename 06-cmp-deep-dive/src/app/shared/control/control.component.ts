2cb374a9590556eb978eea99a35f5647c6caeebc
Yiğit Şevki Kaplan <yigitkaplan2004@hotmail.com>
2025-09-01 10:14:17 +0300

    progress: section 6/116 completed, @HostBinding & @HostListener

--- 06-cmp-deep-dive/src/app/shared/control/control.component.ts
+++ 06-cmp-deep-dive/src/app/shared/control/control.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  HostBinding,
+  HostListener,
+  Input,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -8,9 +14,19 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
   host: {
-    class: 'control'
-  }
+    class: 'control',
+    '(click)': 'onClick()',
+  },
 })
 export class ControlComponent {
-  @Input({required: true}) label!: string;
+  @Input({ required: true }) label!: string;
+
+  // @HostBinding('class') className = 'control'; - same as host: { class: 'control' }
+  // @HostListener('click') onClick() {
+  //   console.log('Control clicked!');
+  // } - same as host: { '(click)': 'onClick()' } also contains onClick() func.
+
+  onClick() {
+    console.log('Control clicked!');
+  }
 }
