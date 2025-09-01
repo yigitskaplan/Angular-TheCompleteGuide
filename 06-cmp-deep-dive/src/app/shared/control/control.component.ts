c2ab5406e325eed1133644b2f8e6a70fd945c0bd
Yiğit Şevki Kaplan <yigitkaplan2004@hotmail.com>
2025-09-01 09:16:08 +0300

    progress: section 6/109 completed, multi-element custom components & content projection

---
+++ 06-cmp-deep-dive/src/app/shared/control/control.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-control',
+  standalone: true,
+  imports: [],
+  templateUrl: './control.component.html',
+  styleUrl: './control.component.css'
+})
+export class ControlComponent {
+  @Input({required: true}) label!: string;
+  
+}
