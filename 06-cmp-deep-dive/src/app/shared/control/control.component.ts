45ce206b914aca6df114f860c41413c82eb2ebef
Yiğit Şevki Kaplan <yigitkaplan2004@hotmail.com>
2025-09-01 09:42:55 +0300

    progress: section 6/112 completed, view encapsulation & host elements

--- 06-cmp-deep-dive/src/app/shared/control/control.component.ts
+++ 06-cmp-deep-dive/src/app/shared/control/control.component.ts
@@ -1,13 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.css'
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None
 })
 export class ControlComponent {
   @Input({required: true}) label!: string;
-  
 }
