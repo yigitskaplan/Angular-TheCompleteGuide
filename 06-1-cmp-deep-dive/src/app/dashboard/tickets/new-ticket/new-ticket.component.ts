89fb9fc8ce12a44998ed04b4647d34ca68da57e8
Yiğit Şevki Kaplan <yigitkaplan2004@hotmail.com>
2025-09-02 15:40:50 +0300

    progress: up to section 6/129 completed, extracting values in a different way with alternative approach

--- 06-1-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 06-1-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -11,8 +11,8 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
-  onSubmit(titleElement: HTMLInputElement) {
-    const enteredTitle = titleElement.value;
-    console.log(enteredTitle);
+  onSubmit(title: string, ticketText: string) {
+    console.log(title);
+    console.log(ticketText);
   }
 }
