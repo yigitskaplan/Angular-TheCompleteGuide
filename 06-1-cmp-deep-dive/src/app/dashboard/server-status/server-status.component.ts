b7389942bcf50a80ef381ddbcc707390e0a83866
Yiğit Şevki Kaplan <yigitkaplan2004@hotmail.com>
2025-09-02 14:54:56 +0300

    progress: up to section 6/126 completed, component cleanup with DestroyRef

--- 06-1-cmp-deep-dive/src/app/dashboard/server-status/server-status.component.ts
+++ 06-1-cmp-deep-dive/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,15 +7,15 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent implements OnInit, OnDestroy{
+export class ServerStatusComponent implements OnInit {
   currentStatus: 'online' | 'offline' | 'unknown' = 'online';
-  private interval?: ReturnType<typeof setTimeout>;
+  private destroyRef = inject(DestroyRef);
 
   // constructor() {} - prefer ngOnInit
 
   ngOnInit() {
     console.log("ON INIT")
-   setInterval(() => {
+   const interval = setInterval(() => {
       const rnd = Math.random(); // 0 - 0.99999999999
 
       if (rnd < 0.5) {
@@ -26,14 +26,18 @@ export class ServerStatusComponent implements OnInit, OnDestroy{
         this.currentStatus = 'unknown';
       }
     }, 5000);
+
+    this.destroyRef.onDestroy(() => {
+      clearInterval(interval);
+    });
   }
 
   ngAfterViewInit() {
     console.log("AFTER VIEW INIT");
   }
 
-  ngOnDestroy() {
-    clearTimeout(this.interval);
-  }
+  // ngOnDestroy() {
+  //   clearTimeout(this.interval);
+  // }
 }
 
