0b60a979c283255ae16b8f947d0382612d2da8d6
Yiğit Şevki Kaplan <yigitkaplan2004@hotmail.com>
2025-09-03 13:36:17 +0300

    progress: section 6/132 follow-up, continued work on viewChild signal function

--- 06-1-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 06-1-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -12,14 +12,15 @@ import { FormsModule } from '@angular/forms';
 })
 export class NewTicketComponent {
   // @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
-  private form = viewChild<ElementRef<HTMLFormElement>>('form');
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
 
+  // for multiple elements (array of elements) there is ViewChildren
 
   onSubmit(title: string, ticketText: string, ) {
     console.log(title);
     console.log(ticketText);
 
     // this.form?.nativeElement.reset();
-    this.form()?.nativeElement.reset();
+    this.form().nativeElement.reset();
   }
 }
