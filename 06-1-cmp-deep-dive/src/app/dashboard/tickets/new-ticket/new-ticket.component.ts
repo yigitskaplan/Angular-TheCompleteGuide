16a90e50382c4371d63c6537ab5fb39703a7d9f8
Yiğit Şevki Kaplan <yigitkaplan2004@hotmail.com>
2025-09-04 09:21:51 +0300

    feat(binding): implement two-way binding (section 6/145)

--- 06-1-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 06-1-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,6 +1,17 @@
-import { AfterViewInit, Component, ElementRef, EventEmitter, OnInit, output, Output, viewChild, ViewChild } from '@angular/core';
-import { ButtonComponent } from "../../../shared/button/button.component";
-import { ControlComponent } from "../../../shared/control/control.component";
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  output,
+  Output,
+  signal,
+  viewChild,
+  ViewChild,
+} from '@angular/core';
+import { ButtonComponent } from '../../../shared/button/button.component';
+import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -8,34 +19,35 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
-  styleUrl: './new-ticket.component.css'
+  styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent implements OnInit, AfterViewInit{
+export class NewTicketComponent implements OnInit, AfterViewInit {
   @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
-  
+
   // private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
 
   // for multiple elements (array of elements) there is ViewChildren
-  
+
   // @Output() add = new EventEmitter<{ title: string; request: string }>();
   add = output<{ title: string; text: string }>();
-
-
+  enteredTitle = signal('');
+  enteredText = signal('');
 
   ngOnInit() {
-      console.log('ONINIT');
-      console.log(this.form?.nativeElement);
+    console.log('ONINIT');
+    console.log(this.form?.nativeElement);
   }
 
   ngAfterViewInit() {
-      console.log('AFTER VIEW INIT')
-      console.log(this.form?.nativeElement);
+    console.log('AFTER VIEW INIT');
+    console.log(this.form?.nativeElement);
   }
 
-  onSubmit(title: string, ticketText: string, ) {
-    this.add.emit({ title: title, text: ticketText });
+  onSubmit() {
+    this.add.emit({ title: this.enteredTitle(), text: this.enteredText() });
 
     // this.form?.nativeElement.reset();
-    this.form?.nativeElement.reset();
-  } 
+    this.enteredTitle.set('');
+    this.enteredText.set('');
+  }
 }
