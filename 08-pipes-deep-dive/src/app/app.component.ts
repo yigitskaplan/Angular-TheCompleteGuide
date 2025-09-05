29d86187eb0616a5d964c834b526bb5df84d3dd8
Yiğit Şevki Kaplan <yigitkaplan2004@hotmail.com>
2025-09-05 13:38:38 +0300

    feat(pipes): explain pure and impure pipe execution (section 8/171)

--- 08-pipes-deep-dive/src/app/app.component.ts
+++ 08-pipes-deep-dive/src/app/app.component.ts
@@ -24,5 +24,8 @@ export class AppComponent {
 
   onReset(index: number) {
     this.historicTemperatures[index] = 18;
+    // const newTemps = [...this.historicTemperatures];
+    // newTemps[index] = 18;
+    // this.historicTemperatures = newTemps;
   }
 }
--- 08-pipes-deep-dive/src/app/sort.pipe.ts
+++ 08-pipes-deep-dive/src/app/sort.pipe.ts
@@ -2,7 +2,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
   name: 'sort',
-  standalone: true
+  standalone: true,
+  pure: false,
 })
 export class SortPipe implements PipeTransform {
   transform(value: string[] | number[], direction: 'asc' | 'desc' = 'asc') {
