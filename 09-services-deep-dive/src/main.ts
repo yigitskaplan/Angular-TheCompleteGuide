e59ec695652d5754705a84c2a24cabf24d850b27
Yiğit Şevki Kaplan <yigitkaplan2004@hotmail.com>
2025-09-06 22:30:00 +0300

    feat(services): provide services using multiple approaches (section 9/182)

--- 09-services-deep-dive/src/main.ts
+++ 09-services-deep-dive/src/main.ts
@@ -1,5 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+// import { TasksService } from './app/tasks/tasks.service';
+
+// bootstrapApplication(AppComponent, {
+//   providers: [TasksService]
+// }).catch((err) => console.error(err));
+// Above is the old way of providing a service at the application level
+// Now we can provide it directly in the service with providedIn: 'root' - better way
 
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
