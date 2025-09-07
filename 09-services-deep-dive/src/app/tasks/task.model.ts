c61d1a39ee908b35a0266da36ba57c013add6b71
Yiğit Şevki Kaplan <yigitkaplan2004@hotmail.com>
2025-09-07 08:27:10 +0300

    feat(di): prepare non-class value for injection (section 9/187)

--- 09-services-deep-dive/src/app/tasks/task.model.ts
+++ 09-services-deep-dive/src/app/tasks/task.model.ts
@@ -1,5 +1,23 @@
 export type TaskStatus = 'OPEN' | 'IN_PROGRESS' | 'DONE';
 
+export const TaskStatusOptions: { value: 'open' | 'in-progress' | 'done'; taskStatus: TaskStatus; text: string }[] = [
+  {
+    value: 'open',
+    taskStatus: 'OPEN',
+    text: 'Open'
+  },
+  {
+    value: 'in-progress',
+    taskStatus: 'IN_PROGRESS',
+    text: 'In Progress'
+  },
+  {
+    value: 'done',
+    taskStatus: 'DONE',
+    text: 'Done'
+  }
+]
+
 export interface Task {
   id: string;
   title: string;
