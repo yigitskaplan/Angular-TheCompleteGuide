d104d2d54057d17bbdd88a59a0bb4eafa054688c
Yiğit Şevki Kaplan <yigitkaplan2004@hotmail.com>
2025-09-07 08:04:19 +0300

    feat(services): inject services into other services (section 9/184)

--- 09-services-deep-dive/src/app/tasks/tasks.service.ts
+++ 09-services-deep-dive/src/app/tasks/tasks.service.ts
@@ -1,12 +1,14 @@
-import { Injectable, signal } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 
 import { Task, TaskStatus } from './task.model';
+import { LoggingService } from '../logging.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TasksService {
   private tasks = signal<Task[]>([]);
+  private loggingService = inject(LoggingService);
 
   allTasks = this.tasks.asReadonly();
 
@@ -17,6 +19,7 @@ export class TasksService {
       status: 'OPEN'
     };
     this.tasks.update((oldTasks) => [...oldTasks, newTask]);
+    this.loggingService.log('Task added:' + newTask.title);
   }
 
   updateTaskStatus(taskId: string, newStatus: TaskStatus) {
@@ -26,5 +29,6 @@ export class TasksService {
       }
       return task;
     }));
+    this.loggingService.log('Task status updated: ' + newStatus);
   }
 }
