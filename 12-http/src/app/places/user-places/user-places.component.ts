0210829b8d5b639d9059db4b9d77823b96392e72
Yiğit Şevki Kaplan <yigitkaplan2004@hotmail.com>
2025-09-12 08:31:06 +0300

    feat(http): fetch more data with some code duplication (section 12/227)

--- 12-http/src/app/places/user-places/user-places.component.ts
+++ 12-http/src/app/places/user-places/user-places.component.ts
@@ -1,7 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
+import { HttpClient } from '@angular/common/http';
+import { Place } from '../place.model';
+import { map } from 'rxjs/internal/operators/map';
+import { catchError } from 'rxjs/internal/operators/catchError';
+import { throwError } from 'rxjs/internal/observable/throwError';
 
 @Component({
   selector: 'app-user-places',
@@ -10,5 +15,41 @@ import { PlacesComponent } from '../places.component';
   styleUrl: './user-places.component.css',
   imports: [PlacesContainerComponent, PlacesComponent],
 })
-export class UserPlacesComponent {
+export class UserPlacesComponent implements OnInit {
+  places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
+  error = signal('');
+  private httpClient = inject(HttpClient);
+  // constructor(private httpClient: HttpClient) {},
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit() {
+    this.isFetching.set(true);
+    const subscription = this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/user-places')
+      .pipe(
+        map((resData) => resData.places),
+        catchError((error) => {
+          console.log(error);
+          return throwError(
+            () => new Error('Failed to fetch your favorite places. Please try again later.')
+          );
+        })
+      )
+      .subscribe({
+        next: (places) => {
+          this.places.set(places);
+        },
+        error: (error: Error) => {
+          this.error.set(error.message);
+        },
+        complete: () => {
+          this.isFetching.set(false);
+        },
+      });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 }
