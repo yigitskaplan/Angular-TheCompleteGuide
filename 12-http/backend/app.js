685299d9843d302763c1f75a305e42b67e17f22c
Yiğit Şevki Kaplan <yigitkaplan2004@hotmail.com>
2025-09-12 16:09:31 +0300

    feat(http): follow up - DELETE request error-modal (section 12/224)

--- 12-http/backend/app.js
+++ 12-http/backend/app.js
@@ -68,6 +68,8 @@ app.put("/user-places", async (req, res) => {
 app.delete("/user-places/:id", async (req, res) => {
   const placeId = req.params.id;
 
+  return res.status(500).json();
+
   const userPlacesFileContent = await fs.readFile("./data/user-places.json");
   const userPlacesData = JSON.parse(userPlacesFileContent);
 
